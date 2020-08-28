95cbdfbdf3bccca49f8cd01aa9c5f61c9f526b59
Kenny Seo <kennyyseo@gmail.com>
2020-08-28 10:49:40 -0700

    Added partials

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const methodOverride = require("method-override");
+require("dotenv").config();
 require("./config/database");
 
 const indexRouter = require("./routes/index");
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use("/sneakers", sneakersRouter);
